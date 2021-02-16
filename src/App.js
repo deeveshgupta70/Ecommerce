38d148626c8cadd7c6a7fc8a69c2ee0c88eff7a2
GosWoj <gosiawoojcik@gmail.com>
2021-02-16 17:01:10 +0100

    created pages and set up router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import SingleProduct from "./pages/SingleProduct";
+import Cart from "./pages/Cart";
+import Error from "./pages/Error";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+        <Route path="/products/:id" children={<SingleProduct />} />
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ src/pages/SingleProduct.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SingleProduct() {
+  return (
+    <div>
+      <h1>Single Product page</h1>
+    </div>
+  );
+}
+
+export default SingleProduct;
